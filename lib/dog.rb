754f01b54222ca4b46097566f4a1e27976299280
Joe Jamsky <cba1067950@yahoo.com>
2019-05-14 20:53:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,15 @@
+class Dog 
+  def name 
+    @name
+  end
+  def name=(newName)
+    @name = newName
+  end
+  
+  def breed 
+    @breed
+  end 
+  def breed=(newBreed)
+    @breed = newBreed
+  end
+end
