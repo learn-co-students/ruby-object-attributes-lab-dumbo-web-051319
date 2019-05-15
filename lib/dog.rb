23ac073baa278c901aab2bed90065e8b8f3f0705
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-05-15 15:36:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -14,5 +14,23 @@ class Dog
   def breed=(breed)
   @breed= breed 
   end   
-   
+end 
+
+class Person
+  def name 
+    @name 
+  end 
+  
+  def name=(name)
+  @name = name
+  end
+  
+  def job 
+    @job 
+  end 
+  
+  def job=(job)
+  @job = job
+  end
+  
 end 
